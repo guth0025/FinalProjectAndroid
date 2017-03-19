7d531b3a5cb85285316c34ce84c33a534027dcc9
Jordan <mcke0368@algonquinlive.com>
2017-03-18 22:03:59 -0400

    Basic update of HomeActivity
    -Added Button on Main activity for HomeActivity testing purposes
    -Added strings for button / EditText hint
    
    -Added ListView, EditText and Button on HomeActivity

--- app/src/main/java/guthboss/com/finalproject/HomeActivity.java
+++ app/src/main/java/guthboss/com/finalproject/HomeActivity.java
@@ -1,13 +1,40 @@
 package guthboss.com.finalproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+    ListView items;
+    ArrayList<String> livingRoomItems = new ArrayList<String>();
+    EditText addItemText;
+    Button addItemBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        addItemText = (EditText)findViewById(R.id.itemText);
+        addItemBtn = (Button)findViewById(R.id.addItem);
+        items = (ListView)findViewById(R.id.HomeItems);
+
+        livingRoomItems.add("Lamp 1");
+        livingRoomItems.add("Lamp 2");
+        livingRoomItems.add("Lamp 3");
+        livingRoomItems.add("Television");
+        livingRoomItems.add("Blinds");
+
+        final ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,livingRoomItems);
+        items.setAdapter(itemsAdapter);
+
+
+
+
     }
 }
--- app/src/main/java/guthboss/com/finalproject/MainActivity.java
+++ app/src/main/java/guthboss/com/finalproject/MainActivity.java
@@ -1,14 +1,27 @@
 package guthboss.com.finalproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button livingRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //a
+
+        //Launch to HomeActivity
+        livingRoom = (Button)findViewById(R.id.homeActivity);
+
+        livingRoom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
