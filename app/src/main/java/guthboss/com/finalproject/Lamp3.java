1cf0fce7767b111966b237c9beaea523dd118a8e
Jordan <mcke0368@algonquinlive.com>
2017-03-19 22:09:05 -0400

    -Created new layout for inflater on ListView items -Created activity for each item on list

---
+++ app/src/main/java/guthboss/com/finalproject/Lamp3.java
@@ -0,0 +1,13 @@
+package guthboss.com.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lamp3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lamp3);
+    }
+}
