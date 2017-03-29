ac1e6d31d2f61aa76b09dc149eac3f75ef107eef
Jordan <mcke0368@algonquinlive.com>
2017-03-29 15:10:10 -0400

    - Added DatabaseHelper class - Improved front end of sub-activities

--- app/src/main/java/guthboss/com/finalproject/Lamp3.java
+++ app/src/main/java/guthboss/com/finalproject/Lamp3.java
@@ -1,13 +1,23 @@
 package guthboss.com.finalproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Lamp3 extends AppCompatActivity {
+    Spinner colourPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lamp3);
+
+        colourPicker = (Spinner)findViewById(R.id.spinner);
+
+        //Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.colours, android.R.layout.simple_spinner_dropdown_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        colourPicker.setAdapter(spinnerAdapter);
     }
 }
